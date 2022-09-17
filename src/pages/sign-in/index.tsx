e2ab4cdc4d7edfe96314ee452939e2105be1082e
hasanaktas <developer@hasanaktas.com>
2022-09-17 11:53:14 +0300

    update src/pages/sign-in/index.tsx

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -6,6 +6,7 @@ import * as yup from 'yup'
 import clsx from 'clsx'
 import { useAuthSignInMutation } from '@/hooks'
 import { GuestGuard } from '@/components'
+import Brand from '@/components/brand'
 
 type LoginForm = {
     email: string
@@ -37,50 +38,57 @@ const AdminSignInPage: NextPageExtended = () => {
     }, [])
 
     return (
-        <Container>
-            <Form onSubmit={formik.handleSubmit} noValidate>
-                <Form.Group className="mb-3">
-                    <Form.Label>Email Address</Form.Label>
-                    <Form.Control
-                        name="email"
-                        value={formik.values.email}
-                        onChange={formik.handleChange}
-                        type="email"
-                        placeholder="Enter Email"
-                        autoComplete="email"
-                        disabled={formik.isSubmitting}
-                        className={clsx({ 'is-invalid': formik.touched.email && Boolean(formik.errors.email) })}
-                    />
-                    <Form.Text className="text-danger">{formik.touched.email && formik.errors.email}</Form.Text>
-                </Form.Group>
-                <Form.Group className="mb-3">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        name="password"
-                        value={formik.values.password}
-                        onChange={formik.handleChange}
-                        type="password"
-                        placeholder="Enter Password"
-                        autoComplete="current-password"
-                        disabled={formik.isSubmitting}
-                        className={clsx({ 'is-invalid': formik.touched.password && Boolean(formik.errors.password) })}
-                    />
-                    <Form.Text className="text-danger">{formik.touched.password && formik.errors.password}</Form.Text>
-                </Form.Group>
-                <Button variant="primary" type="submit" disabled={formik.isSubmitting}>
-                    {formik.isSubmitting && (
-                        <Spinner
-                            as="span"
-                            animation="border"
-                            size="sm"
-                            role="status"
-                            aria-hidden="true"
-                            className="me-2"
+        <Container className="my-4">
+            <div className="bg-white p-5 rounded-2 shadow-sm">
+                <Brand />
+                <Form onSubmit={formik.handleSubmit} noValidate>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Email Address</Form.Label>
+                        <Form.Control
+                            name="email"
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
+                            type="email"
+                            placeholder="Enter Email"
+                            autoComplete="email"
+                            disabled={formik.isSubmitting}
+                            className={clsx({ 'is-invalid': formik.touched.email && Boolean(formik.errors.email) })}
                         />
-                    )}
-                    Submit
-                </Button>
-            </Form>
+                        <Form.Text className="text-danger">{formik.touched.email && formik.errors.email}</Form.Text>
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            name="password"
+                            value={formik.values.password}
+                            onChange={formik.handleChange}
+                            type="password"
+                            placeholder="Enter Password"
+                            autoComplete="current-password"
+                            disabled={formik.isSubmitting}
+                            className={clsx({
+                                'is-invalid': formik.touched.password && Boolean(formik.errors.password),
+                            })}
+                        />
+                        <Form.Text className="text-danger">
+                            {formik.touched.password && formik.errors.password}
+                        </Form.Text>
+                    </Form.Group>
+                    <Button variant="primary" type="submit" disabled={formik.isSubmitting}>
+                        {formik.isSubmitting && (
+                            <Spinner
+                                as="span"
+                                animation="border"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                                className="me-2"
+                            />
+                        )}
+                        Login
+                    </Button>
+                </Form>
+            </div>
         </Container>
     )
 }
